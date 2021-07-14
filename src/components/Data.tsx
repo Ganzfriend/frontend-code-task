17842dd469ba64f152fa8a84228157febd94ac0b
Ganzfriend <melissaganzfried@gmail.com>
2021-07-14 14:14:44 +0200

    parsing api data and mapping in component

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -12,7 +12,29 @@ const Data: React.FC = () => {
   }, []);
 
   return (
-    <div></div>
+    <div>
+      {!!data.length && data.map(({name, stats}) => {
+        const {row_count, keys, categories} = stats;
+        return (
+          <div>
+            <h5>{name}</h5>
+            <h6>Keys: </h6>
+            {!!keys.length ? (
+              keys.map(({id, label, null_fraction, distinct: _distinct}) => {
+                const distinct = _distinct > 0 ? _distinct : Math.abs(_distinct) * row_count;
+              return (
+              <div>
+                {label}
+                Id: {id}
+              </div>
+              )
+            }))
+              : <p> No joining keys available </p>
+            }
+          </div>
+        )
+      })}
+    </div>
   );
 };
 
